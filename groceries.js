1d7485e6534730dadd493a9cecc0b3b2ed38bb90
DESKTOP-DE35VBN\Cesar Ramos <cramos26@mail.csuchico.edu>
2016-11-07 16:29:08 -0800

    Removed value in second line of function additem() to make it a string. Works now!

--- groceries.js
+++ groceries.js
@@ -1,7 +1,7 @@
 
 function addItem(){
   var input = document.getElementById("newItem").value;
-  var list = document.getElementById("listDisplay").value;
+  var list = document.getElementById("listDisplay");
   var item = document.createElement("li");
   var itemName = document.createTextNode(input);
   item.appendChild(itemName);
